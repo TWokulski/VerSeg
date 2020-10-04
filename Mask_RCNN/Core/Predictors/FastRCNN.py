2e31f25281dd6a7fc03934cb7a5326c7314766ad
Tomasz <tom.g2807@gmail.com>
2020-10-04 13:01:45 +0200

    setting FastRCNNPredictor class

--- Mask_RCNN/Core/Predictors/FastRCNN.py
+++ Mask_RCNN/Core/Predictors/FastRCNN.py
@@ -3,4 +3,18 @@ import torch.nn.functional as F
 
 
 class FastRCNNPredictor(nn.Module):
+    def __init__(self, in_channels, mid_channels, num_classes):
+        super().__init__()
+        self.fc1 = nn.Linear(in_channels, mid_channels)
+        self.fc2 = nn.Linear(mid_channels, mid_channels)
+        self.cls_score = nn.Linear(mid_channels, num_classes)
+        self.bbox_prediction = nn.Linear(mid_channels, num_classes * 4)
 
+    def forward(self, x):
+        x = x.flatten(start_dim=1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        score = self.cls_score(x)
+        bbox_delta = self.bbox_prediction(x)
+
+        return score, bbox_delta
