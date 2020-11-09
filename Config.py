ce0d4833d88bb603943ace4d3a26bb9907112511
Tomasz <tom.g2807@gmail.com>
2020-11-09 21:41:14 +0100

    Updating Training without GUI

--- Config.py
+++ Config.py
@@ -1,16 +1,21 @@
-using_cuda = False
-learning_rate = 0.00125
-learning_rate_steps = [40, 70]
-learning_rate_lambda = 0.1
-momentum = 0.9
-decay = 0.0002
-number_of_epochs = 80
-iterations = 25
-publishing_losses_frequency = 100
-ckpt_path = './ckpt'
-dataset_dir = './Dataset'
-seed = 1234
 
-number_of_classes = 2
+class Configuration:
+    def __init__(self):
+        self.using_cuda = False
+        self.device = 'cuda'
+        self.learning_rate = 0.00125
+        self.learning_rate_steps = [40, 70]
+        self.learning_rate_lambda = 0.1
+        self.momentum = 0.9
+        self.decay = 0.0002
+        self.number_of_epochs = 80
+        self.iterations = 25
+        self.publishing_losses_frequency = 100
+        self.ckpt_path = 'ckpt'
+        self.dataset_dir = 'Dataset'
+        self.seed = 1234
+        self.learning_epoch = 0.1
+        self.number_of_iterations = 10
+        self.number_of_classes = 2
 
 
--- Train.py
+++ Train.py
@@ -5,60 +5,102 @@ import re
 import time
 import torch
 import Mask_RCNN as algorithm
-from Config import *
+from Config import Configuration
 
 
 def main():
-    device = torch.device("cuda" if torch.cuda.is_available() and using_cuda else "cpu")
-    print("\ndevice: {}".format(device))
 
-    train_set = algorithm.datasets(dataset_dir, "train2017", train=True)
+    cfg = Configuration()
+    parameters = {
+        "seed": cfg.seed,
+        "number_of_epochs": cfg.number_of_epochs,
+        "number_of_classes": cfg.number_of_classes,
+        "number_of_iterations": cfg.number_of_iterations,
+        "momentum": cfg.momentum,
+        "decay": cfg.decay,
+        "learning_rate": cfg.learning_rate,
+        "learning_steps": cfg.learning_rate_steps,
+        "device": cfg.device,
+        "dataset_dir": cfg.dataset_dir,
+        "publishing_losses_frequency": 100,
+        "checkpoint_path": cfg.ckpt_path,
+        "learning_rate_lambda": cfg.learning_rate_lambda,
+        "model_path": 'model/',
+        "iterations_to_warmup": 800,
+        "result_path": 'ckpt/result'
+    }
+
+    device = torch.device(parameters['device'])
+
+    train_set = algorithm.COCODataset(parameters['dataset_dir'], "Train", train=True)
     indices = torch.randperm(len(train_set)).tolist()
     train_set = torch.utils.data.Subset(train_set, indices)
 
-    val_set = algorithm.datasets(dataset_dir, "val2017", train=True)
-    model = algorithm.resnet50_for_mask_rcnn(True).to(device)
+    val_set = algorithm.COCODataset(parameters['dataset_dir'], "Validation", train=True)
+    model = algorithm.resnet50_for_mask_rcnn(True, parameters['number_of_classes']).to(device)
 
     params = [p for p in model.parameters() if p.requires_grad]
     optimizer = torch.optim.SGD(
-        params, lr=learning_rate, momentum=momentum, weight_decay=decay)
-    decrease = lambda x: learning_rate_lambda ** bisect.bisect(learning_rate_steps, x)
+        params, lr=parameters['learning_rate'],
+        momentum=parameters['momentum'],
+        weight_decay=parameters['decay'])
+
+    decrease = lambda x: parameters['learning_rate_lambda'] ** bisect.bisect(
+        parameters['learning_steps'], x)
 
     starting_epoch = 0
+    prefix, ext = os.path.splitext(parameters['checkpoint_path'])
+    checkpoints = glob.glob(prefix + "-*" + ext)
+    checkpoints.sort(key=lambda x: int(re.search(r"-(\d+){}".format(ext), os.path.split(x)[1]).group(1)))
+    if checkpoints:
+        checkpoint = torch.load(checkpoints[-1], map_location=device)
+        model.load_state_dict(checkpoint["model"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        starting_epoch = checkpoint["epochs"]
+        del checkpoint
+        torch.cuda.empty_cache()
 
     since = time.time()
-    print("\nalready trained: {} epochs; to {} epochs".format(starting_epoch, number_of_epochs))
+    print("\nalready trained: {} epochs; to {} epochs".format(starting_epoch, parameters['number_of_epochs']))
 
-    for epoch in range(starting_epoch, number_of_epochs):
+    for epoch in range(starting_epoch, parameters['number_of_epochs']):
         print("\nepoch: {}".format(epoch + 1))
 
         training_epoch_time = time.time()
-        lr_epoch = decrease(epoch) * learning_rate
-        print("lr_epoch: {:.4f}, factor: {:.4f}".format(lr_epoch, decrease(epoch)))
-        algorithm.train_one_epoch(model, optimizer, train_set, device, epoch)
+        parameters['learning_epoch'] = decrease(epoch) * parameters['learning_rate']
+
+        algorithm.train_epoch(model, optimizer, train_set, device, epoch, parameters)
         training_epoch_time = time.time() - training_epoch_time
+        print('training_epoch_time: ', training_epoch_time)
 
         validation_epoch_time = time.time()
-        eval_output, iter_eval = algorithm.evaluate(model, val_set, device)
+        eval_output = algorithm.evaluate(model, val_set, device, parameters)
         validation_epoch_time = time.time() - validation_epoch_time
+        print('validation_epoch_time: ', validation_epoch_time)
 
         trained_epoch = epoch + 1
-        print("training: {:.2f} s, evaluation: {:.2f} s".format(training_epoch_time, validation_epoch_time))
-        print(eval_output.get_AP())
+        maskAP = eval_output.get_AP()
+        maskAR = eval_output.get_AR()
+        print('AP: ', maskAP)
+        print('AR: ', maskAR)
+        if maskAP['mask AP'] > best_model_by_maskAP:
+            best_model_by_maskAP = maskAP['mask AP']
+            algorithm.save_best(model, optimizer, trained_epoch,
+                                parameters['model_path'], eval_info=str(eval_output))
 
-        algorithm.save_ckpt(model, optimizer, trained_epoch, eval_info=str(eval_output))
+        algorithm.save_checkpoint(model, optimizer, trained_epoch,
+                                  parameters['checkpoint_path'], eval_info=str(eval_output))
 
-        prefix, ext = os.path.splitext(ckpt_path)
-        ckpts = glob.glob(prefix + "-*" + ext)
-        ckpts.sort(key=lambda x: int(re.search(r"-(\d+){}".format(ext), os.path.split(x)[1]).group(1)))
+        prefix, ext = os.path.splitext(parameters['checkpoint_path'])
+        checkpoints = glob.glob(prefix + "-*" + ext)
+        checkpoints.sort(key=lambda x: int(re.search(r"-(\d+){}".format(ext), os.path.split(x)[1]).group(1)))
         n = 3
-        if len(ckpts) > n:
-            for i in range(len(ckpts) - n):
-                os.remove("{}".format(ckpts[i]))
+        if len(checkpoints) > n:
+            for i in range(len(checkpoints) - n):
+                os.remove("{}".format(checkpoints[i]))
 
-    print("\ntotal time of this training: {:.2f} s".format(time.time() - since))
-    if starting_epoch < number_of_epochs:
-        print("already trained: {} epochs\n".format(trained_epoch))
+    total_training_time = time.time() - since
+    print('Total time: ', total_training_time)
 
 
 if __name__ == "__main__":
