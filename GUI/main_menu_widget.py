8f7bb2307ea463b24fe7cb271c0eca7f05e3b823
Tomasz <tom.g2807@gmail.com>
2020-11-30 18:37:08 +0100

    some main menu refactoring, requirements.txt updating

--- GUI/main_menu_widget.py
+++ GUI/main_menu_widget.py
@@ -15,26 +15,26 @@ class MainMenuWidget(QWidget):
         self.title = QLabel("VerSeg", self)
         self.title.setWordWrap(True)
         self.title.setFont(QFont('Arial', 60))
-        self.title.setGeometry(40, 20, 400, 120)
+        self.title.setGeometry(312, 80, 400, 120)
         self.title.setStyleSheet("font-style: italic;\n"
                                  "font-weight: bold;")
 
-        self.segmentation_button = QPushButton("Find vertebra", self)
-        self.segmentation_button.setGeometry(172, 360, 320, 60)
+        self.segmentation_button = QPushButton("Evaluate your model", self)
+        self.segmentation_button.setGeometry(162, 300, 700, 60)
         self.segmentation_button.setFont(QFont('Arial', 20))
         self.segmentation_button.setStyleSheet("background-color:white;\n"
                                                "color: black;\n"
                                                "font-weight: bold;"
                                                "")
         self.training_button = QPushButton("Create your model", self)
-        self.training_button.setGeometry(532, 360, 320, 60)
+        self.training_button.setGeometry(162, 400, 700, 60)
         self.training_button.setFont(QFont('Arial', 20))
         self.training_button.setStyleSheet("background-color:white;\n"
                                            "color: black;\n"
                                            "font-weight: bold;"
                                            "")
-        self.prediction_button = QPushButton("Test", self)
-        self.prediction_button.setGeometry(532, 560, 320, 60)
+        self.prediction_button = QPushButton("Run segmentation without annotations", self)
+        self.prediction_button.setGeometry(162, 500, 700, 60)
         self.prediction_button.setFont(QFont('Arial', 20))
         self.prediction_button.setStyleSheet("background-color:white;\n"
                                            "color: black;\n"
